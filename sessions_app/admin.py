69579139cc55a2583611d1595f456a7f9c310594
879372 <kaios5028@gmail.com>
2026-06-12 02:24:11 -0300

    feat: register models in django admin

---
+++ sessions_app/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Company, Plan, PaymentMethod, Child, Session
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "phone", "whatsapp_instance", "created_at")
+    search_fields = ("name", "slug", "phone")
+    readonly_fields = ("id", "created_at")
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "price", "duration_minutes")
+    list_filter = ("company",)
+
+@admin.register(PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ("name", "company")
+    list_filter = ("company",)
+
+@admin.register(Child)
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ("name", "company", "guardian_name", "guardian_phone")
+    list_filter = ("company",)
+    search_fields = ("name", "guardian_name", "guardian_phone")
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ("child", "company", "status", "start_time", "end_time")
+    list_filter = ("company", "status")
+    search_fields = ("child__name",)
